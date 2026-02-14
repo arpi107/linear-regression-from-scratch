03db03de302f4233001301aafc8e2c17d53fc999
arpi107 <109523488+arpi107@users.noreply.github.com>
2026-02-14 14:36:34 +0530

    Update linear_regression.py
    Added prediction logic with initial parameters

--- linear_regression.py
+++ linear_regression.py
@@ -5,3 +5,17 @@ y = [2,4,6,8,10]
 
 print("Input (x):", x)
 print("Output (y):", y)
+
+# Step 2: Initialize parameters
+m = 0
+b = 0
+
+# Step 3: Make predictions
+predictions = []
+
+for i in x:
+    p = m * i + b
+    predictions.append(p)
+
+
+print("Predictions: ",predictions)
